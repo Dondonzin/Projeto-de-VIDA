f7e2d1577b6650a5588d0a1b2e6f9c42d388d0a6
Dondonzin <150681826+Dondonzin@users.noreply.github.com>
2025-06-25 12:03:47 +0000

    index

---
+++ js-projeto-de-vida-codigo-base/main.js
@@ -0,0 +1,7 @@
+const botoes = document.querySelectorAll (".botao");
+
+for (let i=0; i<botoes.lenght; i++){
+     botoes[i].onclick =function () {
+        
+      for (let j=0; j<botoes.lenght; j++)
+
